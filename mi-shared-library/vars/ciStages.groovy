6c5bce8cf91e2c7a4153c64766550b864c5c6722
embolao <corneigh@gmail.com>
2025-05-25 13:10:36 +0200

    Update ciStages.groovy

--- mi-shared-library/vars/ciStages.groovy
+++ mi-shared-library/vars/ciStages.groovy
@@ -1,30 +1,4 @@
-// vars/ciStages.groovy
-
-def setupPython(Map args = [:]) {
-    echo "Setting up Python ${args.pythonVersion}, venv at ${args.venvDir}"
-    sh """
-        python${args.pythonVersion} -m venv ${args.venvDir}
-        source ${args.venvDir}/bin/activate
-        pip install --upgrade pip
-        pip install -r requirements.txt
-    """
+def hello() {
+  echo "Hola desde la librería compartida"
 }
-
-def lintPython(Map args = [:]) {
-    echo "Running lint"
-    sh """
-        source ${args.venvDir}/bin/activate
-        flake8 .
-    """
-}
-
-def testPython(Map args = [:]) {
-    echo "Running tests"
-    sh """
-        source ${args.venvDir}/bin/activate
-        pytest
-    """
-}
-
 return this
-
