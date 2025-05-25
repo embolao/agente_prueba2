8fa3f6e28503c29887739a593b1ab02742985cd5
embolao <corneigh@gmail.com>
2025-05-25 19:09:17 +0200

    Update runTests.groovy

--- mi-shared-library/vars/runTests.groovy
+++ mi-shared-library/vars/runTests.groovy
@@ -2,11 +2,12 @@ def call() {
     echo "Ejecutando tests..."
 
     sh '''
-        . venv/bin/activate
-        cd agente_prueba2
-        export PYTHONPATH=$(pwd)
-        pytest tests/
-    '''
+    . venv/bin/activate
+    cd agente_prueba2
+    export PYTHONPATH=$(pwd)
+    pytest
+'''
+
 }
 
 
