49bdd1282ee1127cb598293a74734d3e21b99716
embolao <corneigh@gmail.com>
2025-05-25 19:27:00 +0200

    Update runTests.groovy

--- mi-shared-library/vars/runTests.groovy
+++ mi-shared-library/vars/runTests.groovy
@@ -1,20 +1,8 @@
 def call() {
-    echo "Ejecutando tests..."
-
     sh '''
-        # Configurar entorno
-        export PYTHONPATH=$(pwd)
-        . venv/bin/activate  # Usar punto en lugar de source
-        
-        # Ver estructura de archivos (debug)
-        echo "Estructura del directorio:"
-        ls -la agente_prueba2/
-        
-        # Ejecutar pytest con verbosidad
-        python -m pytest agente_prueba2/tests -v
-        
-        # Opcional: desactivar entorno
-        deactivate
+        export PYTHONPATH=$(pwd)/agente_prueba2
+        . venv/bin/activate
+        python -m pytest agente_prueba2/agente_prueba2/tests -v
     '''
 }
 
