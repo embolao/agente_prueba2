ef9750605134c2fccaf9db0cb7b515144602ff6d
embolao <corneigh@gmail.com>
2025-05-25 19:23:04 +0200

    Update runTests.groovy

--- mi-shared-library/vars/runTests.groovy
+++ mi-shared-library/vars/runTests.groovy
@@ -2,9 +2,14 @@ def call() {
     echo "Ejecutando tests..."
 
     sh '''
-        # Activar entorno virtual de forma segura
-        export PYTHONPATH=$(pwd)/agente_prueba2
-        venv/bin/python3 -m pytest agente_prueba2/tests
+        # Configurar entorno
+        export PYTHONPATH=$(pwd)  # Apunta al directorio raíz (donde está venv/)
+        source venv/bin/activate  # Activa el entorno virtual
+        
+        # Ejecutar pytest desde la raíz, apuntando a la carpeta tests
+        python -m pytest agente_prueba2/tests -v
+        
+        deactivate  # Desactiva el entorno (opcional)
     '''
 }
 
