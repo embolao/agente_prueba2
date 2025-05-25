a1a9f728cdc77a72f59cd58605e3061981e3c669
embolao <corneigh@gmail.com>
2025-05-25 11:39:10 +0200

    Update ciStages.groovy

--- .jenkins/vars/ciStages.groovy
+++ .jenkins/vars/ciStages.groovy
@@ -1,3 +1,7 @@
-def hello() {
-    echo "Hola desde la librería compartida!"
+def setupPython(Map args = [:]) {
+    def pythonVersion = args.get('pythonVersion', '3.10.0')
+    def venvDir = args.get('venvDir', '.venv')
+    echo "Instalando Python versión ${pythonVersion} en ${venvDir}"
+    // Aquí iría la lógica real, por ejemplo, con sh
 }
+
